e054e7265fcc5c3c5fe2bc73faf1bd40e0a7e670
KingChad127 <abhinavc@cs.utexas.edu>
2022-01-21 00:04:04 -0600

    added some tests

--- linked_int_list.c
+++ linked_int_list.c
@@ -19,7 +19,7 @@ struct node {
 /**
  * Set the next node
  * @param curr pointer to the node to modify
- * @param next pointer to the node to set as next
+ * @param next pointer to the node to linked_int_list_set as next
  */
 void node_set_next(struct node *curr, struct node *next) {
     curr->next = next;
@@ -28,7 +28,7 @@ void node_set_next(struct node *curr, struct node *next) {
 /**
  * Set the previous node
  * @param curr pointer to the node to modify
- * @param prev pointer to the node to set as prev
+ * @param prev pointer to the node to linked_int_list_set as prev
  */
 void node_set_prev(struct node *curr, struct node *prev) {
     curr->prev = prev;
@@ -65,26 +65,11 @@ struct linked_int_list *create_new_list() {
 }
 
 /**
- * Get the node at a certain position
- * O(N) runtime
- * @param position the position of the node pointer to return
- * @param list the list to traverse through
- * @return a pointer to the node that was requested
- */
-struct node *get_node_at_pos(int position, struct linked_int_list *list) {
-    struct node *tmp = list->header;
-    for (int i = -1; i < position; ++i) {
-        tmp = tmp->next;
-    }
-    return tmp;
-}
-
-/**
  * Add an element to the list
- * @param value the value to add
- * @param list pointer to the list to add to
+ * @param list pointer to the list to linked_int_list_add to
+ * @param value the value to linked_int_list_add
  */
-void add(int value, struct linked_int_list *list) {
+void linked_int_list_add(struct linked_int_list *list, int value) {
     list->size++;
     struct node *n = (struct node *) malloc(node_size);
     n->value = value;
@@ -95,14 +80,29 @@ void add(int value, struct linked_int_list *list) {
 }
 
 /**
- * Insert a new value at a specific index
- * @param value to insert
- * @param pos position to insert at
- * @param list list to insert into
+ * Get the node at a certain position
+ * O(N) runtime
+ * @param list the list to traverse through
+ * @param position the position of the node pointer to return
+ * @return a pointer to the node that was requested
  */
-void insert(int value, int pos, struct linked_int_list *list) {
+struct node *linked_int_list_get_node_at_pos(struct linked_int_list *list, int position) {
+    struct node *tmp = list->header;
+    for (int i = -1; i < position; ++i) {
+        tmp = tmp->next;
+    }
+    return tmp;
+}
+
+/**
+ * Insert a new value at a specific index
+ * @param list list to linked_int_list_insert into
+ * @param value to linked_int_list_insert
+ * @param pos position to linked_int_list_insert at
+ */
+void linked_int_list_insert(struct linked_int_list *list, int pos, int value) {
     list->size++;
-    struct node *prev = get_node_at_pos(pos - 1, list);
+    struct node *prev = linked_int_list_get_node_at_pos(list, pos - 1);
     struct node *next = prev->next;
     struct node *insert_node = (struct node *) malloc(node_size);
     node_set_prev(insert_node, prev);
@@ -114,13 +114,13 @@ void insert(int value, int pos, struct linked_int_list *list) {
 
 /**
  * Change the value at a certain position without changing the size of the list
+ * @param list list to modify
  * @param pos of the node to modify
  * @param value new value to replace the old value with
- * @param list list to modify
  * @return the value that was replaced
  */
-int set(int pos, int value, struct linked_int_list *list) {
-    struct node *node_to_change = get_node_at_pos(pos, list);
+int linked_int_list_set(struct linked_int_list *list, int pos, int value) {
+    struct node *node_to_change = linked_int_list_get_node_at_pos(list, pos);
     int old_value = node_to_change->value;
     node_to_change->value = value;
     return old_value;
@@ -128,22 +128,22 @@ int set(int pos, int value, struct linked_int_list *list) {
 
 /**
  * Return the value at the given index
- * @param pos return the value at this index
  * @param list list to look through
+ * @param pos return the value at this index
  * @return the value at pos index
  */
-int get(int pos, struct linked_int_list *list) {
-    struct node *n = get_node_at_pos(pos, list);
+int linked_int_list_get(struct linked_int_list *list, int pos) {
+    struct node *n = linked_int_list_get_node_at_pos(list, pos);
     return n->value;
 }
 
 /**
  * Remove the node passed in from the list
- * @param node_to_remove tgt
  * @param list to remove from
+ * @param node_to_remove tgt
  * @return the value contained in the node removed
  */
-int remove_node(struct node *node_to_remove, struct linked_int_list *list) {
+int linked_int_list_remove_node(struct linked_int_list *list, struct node *node_to_remove) {
     list->size--;
     int ret_val = node_to_remove->value;
     struct node *prev = node_to_remove->prev;
@@ -156,45 +156,47 @@ int remove_node(struct node *node_to_remove, struct linked_int_list *list) {
 
 /**
  * Remove an element from the list based on position
- * @param pos position to remove from
  * @param list to remove from
+ * @param pos position to remove from
  * @return the value removed
  */
-int remove_at_pos(int pos, struct linked_int_list *list) {
-    struct node *node_to_remove = get_node_at_pos(pos, list);
-    int ret_val = remove_node(node_to_remove, list);
+int linked_int_list_remove_by_pos(struct linked_int_list *list, int pos) {
+    struct node *node_to_remove = linked_int_list_get_node_at_pos(list, pos);
+    int ret_val = linked_int_list_remove_node(list, node_to_remove);
     return ret_val;
 }
 
 /**
  * Remove the leftmost occurrence of value from the list
- * @param value to remove
  * @param list to remove from
+ * @param value to remove
  * @return true if the list was changed as a result of this function
  */
-bool remove_by_value(int value, struct linked_int_list *list) {
+bool linked_int_list_remove_by_value(struct linked_int_list *list, int value) {
     bool removed = false;
     struct node *tmp = list->header->next;
     while (tmp != list->header && !removed) {
         if (tmp->value == value) {
             removed = true;
-            remove_node(tmp, list);
+            linked_int_list_remove_node(list, tmp);
         } else {
             tmp = tmp->next;
         }
     }
+    return removed;
 }
+
 /**
  * Return the index of the left most occurrence of a value from the list after a certain position
- * @param value to remove
- * @param pos position to start looking from
  * @param list to look through
+ * @param pos position to start looking from
+ * @param value to find
  * @return the index of the value that was found, or -1 if value is not in the list
  */
-int index_pos(int value, int pos, struct linked_int_list *list) {
+int linked_int_list_index_pos(struct linked_int_list *list, int pos, int value) {
     int not_found = -1;
     int index = pos;
-    struct node *tmp = list->header->next;
+    struct node *tmp = linked_int_list_get_node_at_pos(list, pos);
     while (tmp != list->header) {
         if (tmp->value == value) {
             return index;
@@ -212,24 +214,24 @@ int index_pos(int value, int pos, struct linked_int_list *list) {
  * @param list to remove from
  * @return the index of the value if found, else return -1
  */
-int index_of(int value, struct linked_int_list *list) {
-    return index_pos(value, 0, list);
+int linked_int_list_index_of(struct linked_int_list *list, int value) {
+    return linked_int_list_index_pos(list, 0, value);
 }
 
 /**
  * Return a sublist of elements in this list from start inclusive to stop exclusive. This list is
  * not changed as a result of this call.
+ * @param list the list from which to create the sublist
  * @param start index of the first element of the sublist
  * @param stop index of the last element of the sublist
- * @param list the list from which to create the sublist
  * @return a list with stop - start elements. The elements are from positions start inclusive to
  * stop exclusive in this list. If stop == start, the returned list is empty
  */
-struct linked_int_list *get_sub_list(int start, int stop, struct linked_int_list *list) {
+struct linked_int_list *linked_int_list_get_sub_list(struct linked_int_list *list, int start, int stop) {
     struct linked_int_list *result = create_new_list();
-    struct node *tmp = get_node_at_pos(start, list);
+    struct node *tmp = linked_int_list_get_node_at_pos(list, start);
     for (int i = start; i < stop; ++i) {
-        add(tmp->value, result);
+        linked_int_list_add(result, tmp->value);
         tmp = tmp->next;
     }
 
@@ -238,16 +240,16 @@ struct linked_int_list *get_sub_list(int start, int stop, struct linked_int_list
 
 /**
  * Remove a range from the list start to stop, inclusive, exclusive
+ * @param list to remove from
  * @param start the first index to remove from
  * @param stop the last index to remove (exclusive)
- * @param list to remove from
  */
-void remove_range(int start, int stop, struct linked_int_list *list) {
-    struct node *remove = get_node_at_pos(start, list);
+void linked_int_list_remove_range(struct linked_int_list *list, int start, int stop) {
+    struct node *remove = linked_int_list_get_node_at_pos(list, start);
     struct node *next = remove->next;
     int size = stop - start;
     for (int i = 0; i < size; ++i) {
-        remove_node(remove, list);
+        linked_int_list_remove_node(list, remove);
         remove = next;
         next = next->next;
     }
@@ -257,13 +259,13 @@ void remove_range(int start, int stop, struct linked_int_list *list) {
  * Remove every node from the list and free the memory of these unused nodes
  * @param list to remove from
  */
-void make_empty(struct linked_int_list *list) {
-    remove_range(0, list->size, list);
+void linked_int_list_make_empty(struct linked_int_list *list) {
+    linked_int_list_remove_range(list, 0, list->size);
 }
 
 /**
- * Print a linked list given the first node of the list
- * @param first node of the linked list
+ * Print a linked list given a pointer to the list
+ * @param list the list to print
  */
 void print_list(struct linked_int_list *list) {
     printf("[ ");
@@ -277,4 +279,92 @@ void print_list(struct linked_int_list *list) {
         tmp = tmp->next;
     }
     printf("]\n");
+}
+
+int main() {
+    // some basic test for this linked_int_list
+
+    // test 1: create new list
+    struct linked_int_list *list = create_new_list();
+    printf("test 1: create new list\n");
+    print_list(list); // expected: [ ]
+
+    // test 2: linked_int_list_add
+    linked_int_list_add(list, 0);
+    linked_int_list_add(list, 1);
+    linked_int_list_add(list, 2);
+    linked_int_list_add(list, 3);
+    linked_int_list_add(list, 4);
+    linked_int_list_add(list, 5);
+    linked_int_list_add(list, 7);
+    linked_int_list_add(list, 8);
+    linked_int_list_add(list, 9);
+    printf("test 2: add\n");
+    print_list(list); // expected: [ 0 1 2 3 4 5 7 8 9 ]
+
+    // test 3: linked_int_list_insert
+    linked_int_list_insert(list, 6, 6);
+    printf("test 3: insert\n");
+    print_list(list); // expected: [ 0 1 2 3 4 5 6 7 8 9 ]
+
+    // test 4: linked_int_list_set
+    linked_int_list_set(list, 0, 10);
+    printf("test 4: set\n");
+    print_list(list); // expected: [ 10 1 2 3 4 5 6 7 8 9 ]
+
+    // test 5: linked_int_list_get
+    int actual = linked_int_list_get(list, 6);
+    printf("test 5: get\n");
+    printf("expected: 6, ");
+    printf("actual: %d\n", actual);
+
+    // test 6: remove at pos
+    actual = linked_int_list_remove_by_pos(list, 5);
+    printf("test 6: remove at pos\n");
+    printf("expected: 5, ");
+    printf("actual: %d, list: ", actual);
+    print_list(list); // expected: [ 10 1 2 3 4 6 7 8 9 ]
+
+    // test 7: remove by value
+    bool actual_bool = linked_int_list_remove_by_value(list, 8);
+    printf("test 7: remove by value\n");
+    printf("expected: 1, ");
+    printf("actual: %d, list: ", actual_bool);
+    print_list(list); // expected: [ 10 1 2 3 4 6 7 9 ]
+
+    // test 8: index_pos
+    actual = linked_int_list_index_pos(list, 4, 10);
+    printf("test 8: index from position\n");
+    printf("expected: -1, ");
+    printf("actual: %d\n", actual);
+
+    // test 9: index_of
+    actual = linked_int_list_index_of(list, 10);
+    printf("test 9: index of\n");
+    printf("expected: 0, ");
+    printf("actual: %d\n", actual);
+
+    // test 10: get sub list
+    struct linked_int_list *sub_list = linked_int_list_get_sub_list(list, 1, 4);
+    printf("test 10: get sub list\n");
+    printf("expected: [ 1 2 3 ], ");
+    printf("actual: ");
+    print_list(sub_list);
+
+    // test 11: remove range
+    linked_int_list_remove_range(list, 4, 8);
+    printf("test 11: remove range\n");
+    printf("expected: [ 10 1 2 3 ], ");
+    printf("actual: ");
+    print_list(list);
+
+    // test 12: make empty
+    linked_int_list_make_empty(list);
+    printf("test 12: make empty\n");
+    printf("expected: 0, [ ]; ");
+    printf("actual: %d, ", list->size);
+    print_list(list);
+
+
+    return EXIT_SUCCESS;
 }
